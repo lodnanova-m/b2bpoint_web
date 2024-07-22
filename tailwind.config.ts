7178eb6dec507ace47b65fd54cf1beffa11c3414
Michaela <lodnanovamichaela@gmail.com>
2024-07-22 22:21:38 +0200

    feat: added fonts (HELP)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,27 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      lila: "#A84491",
+      yellow: "#EBE466",
+      blue: "#94C9E1",
+      purple: "#463863",
+      white: "#FFFFFF"
+
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        'avenir': ['Avenir Next Cyr', 'sans-serif'],
       },
+      fontWeight: {
+        'ultralight-italic': "200",
+        'regular': "400",
+        'medium': "500",
+        'bold': "700",
+        'bold-italic': "700",
+        'medium-italic': "500",
+      },
+      
     },
   },
   plugins: [],
