0110621e80b76aa58b423a56483e743b127b5d7a
Michaela <lodnanovamichaela@gmail.com>
2024-07-24 11:33:03 +0200

    feat: partners

--- app/components/Partners/Partners.tsx
+++ app/components/Partners/Partners.tsx
@@ -1,5 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export function Partners() {
     return (
-        <div id="partners">Partners work!</div>
+        <>
+            <div className="flex justify-center font-avenir font-light text-xl mt-20 mb-4 text-blue">NAŠI PARTNEŘI</div>
+            <div id="partners" className="flex flex-row justify-between  bg-blue bg-opacity-30 px-20 py-10">
+                <Link href=""> <Image src="./partners/angle-small-left.svg" alt="left angle icon" width={80} height={80}></Image></Link>
+                <div className="flex flex-row gap-7 items-center justify-center content-center">
+
+                    <Image src="./partners/scukcz.svg" alt="partner1 logo" height={200} width={200}></Image>
+                    <Image src="./partners/ceska_posta.svg" alt="partner2 logo" height={250} width={250}></Image>
+                </div>
+                <Link href=""><Image src="./partners/angle-small-right.svg" alt="right angle icon" width={80} height={80}></Image></Link>
+            </div>
+        </>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Contact } from "./components/Contact/Contact";
 
 export default function Home() {
   return (
-    <div className="flex flex-col px-40">
+    <div className="flex flex-col">
       <About></About>
       <Partners></Partners>
       <Contact></Contact>
